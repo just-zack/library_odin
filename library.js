6f9237ee538243507f7efa6a47ced2bc61c208d4
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-05-02 12:46:12 -1000

    Add modal with add-book form

--- library.js
+++ library.js
@@ -1,3 +1,24 @@
+const body = document.querySelector('body');
+const addBook = document.querySelector('#add_book');
+const modal = document.querySelector('#modal');
+var span = document.querySelector("#close");
+
+addBook.addEventListener('click', openModal);
+span.addEventListener('click', closeModal);
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+function openModal () {
+    modal.style.display = "flex";
+}
+
+function closeModal () {
+    modal.style.display = "none";
+}
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
